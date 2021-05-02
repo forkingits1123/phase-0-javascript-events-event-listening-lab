4cdf7d8cf882aaea8a122292933c7eb002c5535c
forkingits1123 <lblack11235813@gmail.com>
2021-05-01 18:33:29 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 function addingEventListener() {
-}
+
+    const input = document.getElementById('input');
+    input.addEventListener('mousedown', function() {
+        alert('I was clicked!');
+});
+
+} 
+
+addingEventListener();
